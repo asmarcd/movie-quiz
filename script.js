2c0c0195e14304226c0696933631f29cf9c48cfd
Asmar <asmarcd@gmail.com>
2020-09-14 17:05:09 -0700

    HTML and pseudo coding complete

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello");
+
+
+// When start button is clicked: header and instructions go away
+// When start button is clicked: first question comes up
+// When start button is clicked: time begins 60 second countdown
+
+// When a question is answered correctly, move to next question
+// When a question is answered correctly: show "Correct!" on page below next question based on answer
+
+// When a question is answered incorrectly, move to next question
+// When a question is answered incorrectly: show "Wrong!" on page below next question based on answer
+// When a question is answered incorrectly: remove time from clock
+
+// When time runs out, the game ends
+// When all questions are answered, the game ends
+
+// When the game ends, input your initials
+// When the game ends, record the users' score and associate it via object with their intials
+// When initials and score are inputted, also save to local storage
+
+// When high score button is clicked, display high scores from local storage
