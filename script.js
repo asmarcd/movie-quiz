93d999f0126c95f7acfc0210a9455e9fa25a6d21
Asmar <asmarcd@gmail.com>
2020-09-14 18:00:05 -0700

    timer, start page, and start button complete

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
+// Global variables
+const goButton = document.querySelector(".go")
+const title = document.querySelector(".title")
+const intro = document.querySelector(".intro")
+const timer = document.querySelector(".timer")
+const questions = document.querySelector(".questions")
+let secondsLeft = 60
+
+// Process of working through questions
+goButton.addEventListener("click", function () {
+    quizStart();
+})
+
+// When time runs out:
+// if (secondsLeft === 0) {
+//     questions.textContent = "woo!";
+//     title.textcontent = "Time's up!";
+// }
+
+// Functions
+function quizStart() {
+    if (secondsLeft > 0) {
+        title.textContent = "";
+        intro.textContent = "";
+        goButton.style.display = "none";
+    }
+    let countdown = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = `Timer: ${secondsLeft}`
+
+        if (secondsLeft === 0) {
+            questions.textContent = "";
+            title.textContent = "Time's up!";
+            clearInterval(countdown);
+        }
+    }, 1000);
+};
 
 
-// When start button is clicked: header and instructions go away
+
+
+
+
 // When start button is clicked: first question comes up
-// When start button is clicked: time begins 60 second countdown
 
 // When a question is answered correctly, move to next question
 // When a question is answered correctly: show "Correct!" on page below next question based on answer
