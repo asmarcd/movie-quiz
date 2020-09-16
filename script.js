ff175915780cdc3f1cce68cc2a6f200cff8791bf
Asmar <asmarcd@gmail.com>
2020-09-15 18:56:20 -0700

    pre jQuery coding work. Big refactor coming next

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const goButton = document.querySelector(".go");
 const title = document.querySelector(".title");
 const intro = document.querySelector(".intro");
 const timer = document.querySelector(".timer");
-const questionsAll = document.querySelector(".question");
+const questionsAll = document.querySelectorAll(".question");
 const question1 = document.querySelector("#q1");
 const question2 = document.querySelector("#q2");
 const question3 = document.querySelector("#q3");
@@ -11,56 +11,96 @@ const question4 = document.querySelector("#q4");
 const question5 = document.querySelector("#q5");
 let secondsLeft = 60;
 
+// When you want to replace the HTML questions with an array, do it by finishing this. each questions will be myQuestions[i] when you're trying to pull them later:
+const myQuestions = [
+    {
+        question: "Which now famous director made their acting debut in 1999's 'Mystery Men' with the line, 'Can we bring the brewskies?'",
+        a1: "Joss Whedon",
+        a2: "Michael Bay",
+        a3: "Patty Jenkins",
+        a4: "Bret 'The Rat' Ratner"
+    },
+
+    {
+        question: "",
+        a1: "",
+        a2: "",
+        a3: "",
+        a4: "",
+    },
+]
+
+
+
 // Process of working through questions
 goButton.addEventListener("click", function () {
     quizStart();
     askQuestionOne();
 });
 
-// Right now I have this so that it only advances you if you click on the correct button. That's not what I want. I should be able to use the click event delegation (if it's an img, like the exercise from Monday) to make a single formula that moves the quiz forward so long as they click on a button in that html section.
-document.querySelector("#correct1").addEventListener("click", function(){
-    askQuestionTwo();
+question1.addEventListener("click", function (event) {
+    if (event.target.matches(".q1a")) {
+        askQuestionTwo();
+    }
 });
-
-
+question2.addEventListener("click", function (event) {
+    if (event.target.matches(".q2a")) {
+        askQuestionThree();
+    }
+});
+question3.addEventListener("click", function (event) {
+    if (event.target.matches(".q3a")) {
+        askQuestionFour();
+    }
+});
+question4.addEventListener("click", function (event) {
+    if (event.target.matches(".q4a")) {
+        askQuestionFive();
+    }
+});
+question5.addEventListener("click", function (event) {
+    if (event.target.matches(".q5a")) {
+        // insert function for moving to end screen, but without time's up message
+    }
+});
 
 // Functions
 function quizStart() {
-        title.textContent = "";
-        intro.textContent = "";
-        goButton.style.display = "none";
+    title.textContent = "";
+    intro.textContent = "";
+    goButton.style.display = "none";
 
-        let countdown = setInterval(function () {
-            secondsLeft--;
-            timer.textContent = `Timer: ${secondsLeft}`
+    let countdown = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = `Timer: ${secondsLeft}`
 
-            if (secondsLeft === 0) {
-                gameOver();
-                clearInterval(countdown);
-            }
-        }, 1000);
-    };
+        if (secondsLeft === 0) {
+            gameOver();
+            clearInterval(countdown);
+        }
+    }, 1000);
+};
 
 
-    // Is there a way to combine these into a single function with a loop?
+// Is there a way to combine these into a single function with a loop?
 function askQuestionOne() {
-    question1.setAttribute("class", "visible"); 
+    question1.setAttribute("class", "visible");
 };
 function askQuestionTwo() {
-    questionsAll.setAttribute("class", "question"); 
-    question2.setAttribute("class", "visible"); 
+    question1.setAttribute("class", "question");
+    question2.setAttribute("class", "visible");
 };
 function askQuestionThree() {
-    questionsAll.setAttribute("class", "question"); 
-    question3.setAttribute("class", "visible"); 
+    question2.setAttribute("class", "question");
+    question3.setAttribute("class", "visible");
 };
 function askQuestionFour() {
-    questionsAll.setAttribute("class", "question"); 
-    question4.setAttribute("class", "visible"); 
+    question3.setAttribute("class", "question");
+    question4.setAttribute("class", "visible");
 };
 function askQuestionFive() {
-    questionsAll.setAttribute("class", "question"); 
-    question5.setAttribute("class", "visible"); 
+    question4.setAttribute("class", "question");
+    question5.setAttribute("class", "visible");
 };
 
 
